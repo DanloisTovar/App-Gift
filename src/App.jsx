9fa7951651cb0eb8c499deb956f0cc15bed3435c
Danlois Tovar <danlois.tovar@gmail.com>
2023-01-29 19:56:47 -0300

    estructura del componente app.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,15 @@ import { useState } from 'react';
 function App() {
     return (
         <div className="App">
+            {/* titulo */}
             <h1>App Gift</h1>
+            {/* metodo de entrada (input) */}
+
+            {/* lista de items */}
+
+            {/* item */}
+
+            {/* footer */}
         </div>
     );
 }
